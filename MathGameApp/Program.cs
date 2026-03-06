4247784aa053e9c9d7456d8e73f135107b1842a6
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2026-03-06 16:54:13 +0200

    wrote code to clear the console each time the menu reloads, and the first  a few line to create the menu lay out

--- MathGameApp/Program.cs
+++ MathGameApp/Program.cs
@@ -1,6 +1,10 @@
-﻿bool isRunning = true;
+﻿bool isRunning = true; // this is to control whether the menu will keep on looping
 
 while (isRunning)
 {
-    
+    Console.Clear();
+
+    Console.WriteLine("=========================");
+    Console.WriteLine("         MATH MENU       ");
+    Console.WriteLine("=========================");
 }
